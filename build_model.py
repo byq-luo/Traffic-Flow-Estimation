ad1baa5c6598fb8b5b4b176e8596fbaddfb39dd0
Tugberk Ayar <tugberkayar@gmail.com>
2019-04-15 14:56:00 +0300

    documentation on build_model.py

--- build_model.py
+++ build_model.py
@@ -1,58 +1,133 @@
-import pandas as pd
+""" This Module is the base module for constructing LSTM Neural Networks
+	It includes neccesarry functions to prepare data for LSTM
+	Created by Tuğberk AYAR & Ferhat ATLİNAR for 
+	Traffic Flow Estimation with Deep Learning Project. 25 March 2019 Monday"""
+
+
+#Imported libraries for preparing data
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from data_fusion_transformation import divide_hours
 
 def read_data(file_name, parse_dates = ['Date'], index_col = ['Date']):
+    """Reads data from file into dataframe with parsing Date column into datetime object
+
+    :param filename: File to be read
+    :param parse_dates: column to be parse as datetime
+    :param index_col: column name to be parsed
+    :return: dataframe of written data
+    """
     return pd.read_csv(file_name, parse_dates = parse_dates, index_col = index_col)
 
 def scale_Data(df):
-    sc = MinMaxScaler(feature_range = (0, 1))
-    scaled_data = sc.fit_transform(df)
-    return scaled_data, sc
+	""" Normalizes data in order to scale it between 0 and 1
+	
+	:param df: DataFrame which includes data to be scaled
+    :return: scaled data and MinMaxScaler object
+	"""
+	sc = MinMaxScaler(feature_range = (0, 1))
+	scaled_data = sc.fit_transform(df)
+	return scaled_data, sc
 
 def inverse_scale(sc, arr):
+    """ Inverses scaled data
+
+    :param sc: MinMaxScaler object to be used for inversing data
+    :param: arr: data to be inversed
+    :return: inversed data array
+    """
     return sc.inverse_transform(arr)
 
 def merge_one_hot_to_data(df, col):
+    """ Merges one hot vector to dataframe
+    
+    :param df: dataframe that one hot vector will be merged into
+    :param col: column that one hot vector will be created out of it
+    :return: merged dataframe
+    """
     dummy = pd.get_dummies(col.values.ravel())
     dummy.set_index(df.index, inplace = True)
     return pd.merge(dummy, df, 
                     left_index = True, right_index = True)
 
 def join_month_one_hot(df):
+    """ Adds one hot vector of month to dataframe
+
+    :param df: dataframe that one hot vector will be added
+    :return: one hot vector added dataframe
+    """
     months = pd.DataFrame(data = df.index.month, index = df.index)
     return merge_one_hot_to_data(df, months)
 
 def join_weekday_one_hot(df):
+    """ Adds one hot vector of weaakday to dataframe
+
+    :param df: dataframe that one hot vector will be added
+    :return: one hot vector added dataframe
+    """
     days = pd.DataFrame(data = df.index.weekday, index = df.index)
     return merge_one_hot_to_data(df, days)
 
 def join_minute_one_hot(df):
+    """ Adds one hot vector of minute to dataframe
+
+    :param df: dataframe that one hot vector will be added
+    :return: one hot vector added dataframe
+    """
     minute = pd.DataFrame(data = df.index.hour * 24 + df.index.minute / 5,
                          index = df.index)
     return merge_one_hot_to_data(df, minute)
 
 def join_daypart_one_hot(df):
+    """ Adds one hot vector of daypart to dataframe
+
+    :param df: dataframe that one hot vector will be added
+    :return: one hot vector added dataframe
+    """
     hours = pd.DataFrame(data = df.index.map(divide_hours),
                         index = df.index)
     return merge_one_hot_to_data(df, hours)
     
 def join_hour_one_hot(df):
+    """ Adds one hot vector of hour to dataframe
+
+    :param df: dataframe that one hot vector will be added
+    :return: one hot vector added dataframe
+    """
     hours =  pd.DataFrame(data =df.index.hour,
                         index = df.index)
     return merge_one_hot_to_data(df, hour)
 
 def get_month(date):
+    """ Gets month from datetime object
+    
+    :param date: datetime object that month information will be exctract from
+    :return: month of the datatime object
+    """
     return date.month
 
 def find_indexes_of_month(df, month):
+    """ Gets indexes of given month from given dataframe
+
+    :param df: dataframe that month indexes will be exctract from
+    :param month: month that indexes of it will be extracted from dataframe
+    :return: indexes of given month
+    """
     df.reset_index(inplace = True)
     indexes = df.index[df['Date'].apply(get_month) == month].tolist()
     df.set_index(['Date'], drop = True, inplace = True)
     return indexes
 
 def build_sets(df, indexes, distance, time_interval):
+    """ Builds sets for LSTM model as either training or test based on given purpose
+
+    :param df: dataframe to be used to exctract sets out of it
+    :param indexes: indexes that specifies interval of aimed data 
+    :param distance: difference between x set starting index and y set starting index
+    :param time_interval: difference plus one between x set starting index and end index
+    :return: np array of x and y sets
+    """
     x = []
     y = []
     arr = df.values
@@ -62,6 +137,12 @@ def build_sets(df, indexes, distance, time_interval):
     return np.array(x), np.array(y)
 
 def average_estimation(x, y):
+    """ Calculates average MAPE from given sets
+
+    :param x: x set 
+    :param y: y set
+    :return: average MAPE 
+    """
     error = 0
     for i in range(x.shape[0]):
         sum = 0
--- model.py
+++ model.py
@@ -1,28 +1,67 @@
+#importing base module in order to prepare LSTM model
 import build_model as bm
+import pandas as pd
+import numpy as np
 
-file = "preprocessed_471.csv"
+# index values of months (used for given start of sets for test and training)
+JANUARY_INDEX   = 1
+FEBRUARY_INDEX  = 2
+MARCH_INDEX     = 3
+APRIL_INDEX     = 4
+MAY_INDEX       = 5
+JUNE_INDEX      = 6
+JULY_INDEX      = 7
+AUGUST_INDEX    = 8
+SEPTEMBER_INDEX = 9
+OCTOBER_INDEX   = 10
+NOVEMBER_INDEX  = 11
+DECEMBER_INDEX  = 12 
 
-data = bm.read_data(file)
+FILE_NAME = "preprocessed_471.csv"
+PREVIOUS_WEAK_DATA = 2019
+NUMBER_OF_DATA_FROM_PREV_WEAK = 7
+
+#Read and scale data
+data = bm.read_data(FILE_NAME)
+
+#data = data[data.index.month == MARCH_INDEX]
+#data = data[data['Speed'] == -1]
+#print(data.groupby([data.index.day]).count())
+
 data['Scaled'], sc = bm.scale_Data(data)
+
+#add one hots to data
 data = bm.join_weekday_one_hot(data)
 data = bm.join_daypart_one_hot(data)
+
+#drop the speed column which includes real speed values (scaled values will be used instead)
 data.drop(['Speed'], axis = 'columns' ,inplace = True)
 
-indexes = bm.find_indexes_of_month(data, 3)
-indexes.extend(bm.find_indexes_of_month(data, 4))
-print(len(indexes))
-x_train, y_train = bm.build_sets(data, indexes, 2019, 7)
-indexes = bm.find_indexes_of_month(data, 5)
-x_test, y_test = bm.build_sets(data, indexes, 2019, 7)
-x_test = x_test[288:2304,:,:]
-y_test = y_test[288:2304]
-print(x_test.shape)
-print(y_test.shape)
+#build trainig and test sets
+indexes = bm.find_indexes_of_month(data, APRIL_INDEX)
+indexes.extend(bm.find_indexes_of_month(data, MAY_INDEX))
+indexes.extend(bm.find_indexes_of_month(data, JUNE_INDEX))
+x_train, y_train = bm.build_sets(data, indexes, PREVIOUS_WEAK_DATA, NUMBER_OF_DATA_FROM_PREV_WEAK) # 2019 bir hafta sonrası, 7 yarım saat içim
+
+indexes = bm.find_indexes_of_month(data, JULY_INDEX) 
+x_test, y_test = bm.build_sets(data, indexes, PREVIOUS_WEAK_DATA, NUMBER_OF_DATA_FROM_PREV_WEAK)
+
+#one week from test set starting from may 2 (cause may 1 is holiday)
+x_test = x_test[0:2016,:,:]
+y_test = y_test[0:2016]
+
+
+
+#importing keras model and layers to construct LSTM model
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, LSTM, Dropout
 
+#initializing regression model
 regressor = Sequential()
 
+#adding layer(s) to model
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], x_train.shape[2])))
+regressor.add(Dropout(0.5))
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], x_train.shape[2])))
 regressor.add(Dropout(0.5))
 regressor.add(LSTM(units = 33, return_sequences = True, input_shape = (x_train.shape[1], x_train.shape[2])))
@@ -30,5 +69,15 @@ regressor.add(LSTM(units = 33, return_sequences = True, input_shape = (x_train.s
 regressor.add(Flatten())
 regressor.add(Dense(units = 1))
 
+#compiling the model with  mean_absolute_percentage_error and adam optimizer
 regressor.compile(optimizer = 'adam', loss = 'mean_absolute_percentage_error')
-regressor.fit(x_train, y_train, epochs = 20, batch_size = 32, validation_data = (x_test, y_test))
+#fitting model with training sets and validation set
+regressor.fit(x_train, y_train, epochs = 100, batch_size = 32, validation_data = (x_test, y_test))
+results = regressor.predict(x_test)
+
+
+data =  data[data.index.month == JULY_INDEX]
+data =  pd.DataFrame(index = data.index[0:2016], data = sc.inverse_transform(y_test.reshape(-1,1)), columns = ['actual speed'])
+preds = pd.DataFrame(data = sc.inverse_transform(results), columns = ['predicted speed'], index = data.index)
+dt = pd.concat([data, preds], axis = 1)
+dt.to_csv("three_month_train_one_test.csv")
