f8d3d63a16f670cbe7491260d2a2e5fa545971ac
ceatlinar <atlinar21@gmail.com>
2019-04-27 14:57:52 +0300

    Add preprocessed 442 and 1745 for 2016

--- draw_est_graph.py
+++ draw_est_graph.py
@@ -1,17 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import build_model as bm
 
-FILE_NAME = 'preprocessed_471.csv'
+FILE_NAME = "preprocessed_471.csv"
+SECOND_FILE ="Estimations.csv"
+
+# index values of months (used for given start of sets for test and training)
+JAN = 1
+FEB = 2
+MAR = 3
+APR = 4
+MAY = 5
+JUN = 6
+JUL = 7
+AUG = 8
+SEP = 9
+OCT = 10
+NOV = 11
+DEC = 12
 
 
-
-df = pd.read_csv(FILE_NAME,parse_dates = ['Date'], index_col = ['Date'])
-
-df_one = df[df.index.month == 6 ]
-df_one = df_one[df_one.index.day < 10]
+data = bm.read_data(FILE_NAME)
+ests = bm.read_data(SECOND_FILE)
+ests.drop(['Real'], axis='columns', inplace=True)
 
 
+data_prev = data.shift(7*24*12)
+data_prev.columns = ['week_before']
 
+plot_data = pd.concat([data, data_prev,ests], axis =1)
+plot_data = plot_data[plot_data.index.month == JUN]
+plot_data = plot_data[plot_data.index.day < 10]
 
-df_one.plot(y = ['Speed'])
+plot_data.plot(y = ['Speed', 'Predictions'])
+plot_data.plot(y = ['week_before', 'Predictions'])
 plt.show()
+
--- preprocess_sensor_point.py
+++ preprocess_sensor_point.py
@@ -1,7 +1,7 @@
 import data_preprocessing as dpp
 
-FILE_NAME = ".\\CSV Files\\470_2016.csv"
-FILE_NAME_to_be_Saved = "preprocessed_470_2016.csv"
+FILE_NAME = ".\\CSV Files\\1745_0_2016_all_data.csv"
+FILE_NAME_to_be_Saved = "preprocessed_1745_2016.csv"
 
 dpp.preprocess_and_save_data(load_name=FILE_NAME, save_name= FILE_NAME_to_be_Saved,
                              time_window_outlier=20, sigma=2, time_window_downsample=5)
